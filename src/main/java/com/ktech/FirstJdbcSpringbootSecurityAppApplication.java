4a703a4dab4a2c50df0427681f112fe2034c21ca
JavaDevKKT <javadevkkt@gmail.com>
2024-03-03 16:44:05 +0530

    first  cmt

---
+++ src/main/java/com/ktech/FirstJdbcSpringbootSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.ktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJdbcSpringbootSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJdbcSpringbootSecurityAppApplication.class, args);
+	}
+
+}
